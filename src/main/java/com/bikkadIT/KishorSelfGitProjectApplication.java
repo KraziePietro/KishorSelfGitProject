712cab91bf4123ab798152259146f422c7882185
KraziePietro <Kishorlate74@gmail.com>
2022-08-04 18:33:04 +0530

    SMS:01 Student project commited

---
+++ src/main/java/com/bikkadIT/KishorSelfGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KishorSelfGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KishorSelfGitProjectApplication.class, args);
+	}
+
+}
